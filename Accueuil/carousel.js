2fca5095f930b00530aec916da457483b7a25e48
Florent <florentlempro@gmail.com>
2020-10-04 23:09:26 +0200

    Accueil refonte full responsive et changement de style

--- Accueuil/carousel.js
+++ Accueuil/carousel.js
@@ -12,5 +12,5 @@ function showSlides() {
       slideIndex = 1
     }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
+  setTimeout(showSlides, 15000);
 } 
--- Accueuil/nav.js
+++ Accueuil/nav.js
@@ -3,15 +3,12 @@ let state = true;
 
 burgerBtn.addEventListener('click', function(event){
     let toggleMenu = document.querySelector(".menu");
-    let toggleDarkener = document.querySelector(".darkener");
 
     if(state === true) {
         toggleMenu.style.transform = "translateX(0)";
-        toggleDarkener.style.display = "block";
         state = false;
     } else {
         toggleMenu.style.transform = "translateX(-100%)";
-        toggleDarkener.style.display = "none";
         state = true;
     }
 })
