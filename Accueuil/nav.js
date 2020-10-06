edf8a7b01f30bb53f5fe8d385bf6882ca43f0fee
Florent <florentlempro@gmail.com>
2020-10-06 13:55:55 +0200

    navbar animation

--- Accueuil/nav.js
+++ Accueuil/nav.js
@@ -12,3 +12,21 @@ burgerBtn.addEventListener('click', function(event){
         state = true;
     }
 })
+
+let prevScrollPos = window.pageYOffset;
+const navBar = document.querySelector('.navBar');
+
+window.onscroll = function(){
+    let currentScrollPos = window.pageYOffset;
+    let toggleMenu = document.querySelector(".menu");
+    if(prevScrollPos > currentScrollPos) {
+        navBar.style.transform = "translateY(0)";
+
+
+    } else {
+        navBar.style.transform = "translateY(-100%)";
+        toggleMenu.style.transform = "translateX(-100%)";
+
+    }
+    prevScrollPos = currentScrollPos;
+}
